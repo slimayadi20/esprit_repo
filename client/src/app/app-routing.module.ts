ce9701c1b1731986a19b9fc1203ed97e4f4e0021
wajihtl <wajihtlili@esprit.tn>
2021-11-07 20:09:15 +0100

    -feedback  -code optimization  -url about us - too much css

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { AuthGuard } from './auth.guard';
- import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MatiereComponent } from './matiere/matiere.component';
 import { MatieresComponent } from './matieres/matieres.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path:'', redirectTo:'login', pathMatch:'full' },
-  { path:"home" ,   data: {breadcrumb: 'home'}, component:HomeComponent,canActivate:[AuthGuard] },
-  { path:"matieres/:id" ,data: {breadcrumb: 'home'}, component:MatieresComponent,canActivate:[AuthGuard]  },
-  { path:"matieres/:id/:name" , component:MatiereComponent,canActivate:[AuthGuard]  },
-  { path:"aboutus" , component:AboutusComponent,canActivate:[AuthGuard]  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: "home", data: { breadcrumb: 'home' }, component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "matieres/:id", data: { breadcrumb: 'home' }, component: MatieresComponent, canActivate: [AuthGuard] },
+  { path: "matieres/:id/:name", component: MatiereComponent, canActivate: [AuthGuard] },
+  { path: "aboutus", component: AboutusComponent, canActivate: [AuthGuard] },
 
-  { path:"login" , component:LoginComponent  },
-  { path:"**" , component: NotFoundComponent},
-  
+  { path: "login", component: LoginComponent },
+  { path: "**", component: NotFoundComponent },
+
 ];
 
 @NgModule({
